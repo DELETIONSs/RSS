e89798ec27cd7d9557fa86287510b02d57b89ce2
DELETIONSs <officialkadencreations@gmail.com>
2025-10-19 08:50:22 -0400

    Create tokens.lua

---
+++ src/tokens.lua
@@ -0,0 +1,6 @@
+return {
+    primary = Color3.fromRGB(52, 120, 246),
+    secondary = Color3.fromRGB(30, 30, 30),
+    accent = Color3.fromRGB(255, 255, 255),
+    text = Color3.fromRGB(235, 235, 235)
+}
